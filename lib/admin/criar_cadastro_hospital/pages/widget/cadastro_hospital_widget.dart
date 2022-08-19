324f74fed0112f2d6f2cb4f1701a37e88addc075
TiCefops <SistemasCefops@2022>
2022-08-19 10:03:24 -0300

    Bloc da turma

---
+++ lib/admin/criar_cadastro_hospital/pages/widget/cadastro_hospital_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class CadastroHospitalWidget extends StatelessWidget {
+  const CadastroHospitalWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('DDDDDDDDD'),
+    );
+  }
+}
